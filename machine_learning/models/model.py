81db6c1a89e37fceb8319ab3c522083107c77938
Tudor Avram <a96tudor@gmail.com>
2018-11-24 15:12:44 +0200

    finished definition of Model class

--- machine_learning/models/model.py
+++ machine_learning/models/model.py
@@ -2,32 +2,83 @@ import tensorflow as tf
 
 
 class Model:
-    def __init__(self, graph_def, labels):
+    """Class representing a machine learning model used in classification.
+
+    Parameters
+    ----------
+    graph_def: tf.GraphDef
+        With the definition of the model.
+    labels: iterable of str
+        With the labels the model is going to return.
+    name: str
+        The name of the model. Preferably, it should be what this model is
+        supposed to classify.
+
+    """
+    def __init__(self, graph_def, labels, name):
         self._labels = labels
         self._graph_def = graph_def
+        self.name = name
 
     @staticmethod
-    def load_from_pb(pb_file, labels):
-        """
+    def load_from_pb(pb_file, labels, name):
+        """Load model from a .pb file.
 
-        :param pb_file:
-        :param labels:
-        :return:
-        """
-        label_lines = [
-            line.rstrip() for line in tf.gfile.GFile(labels),
-        ]
+        Parameters
+        ----------
+        pb_file: str
+            Path to the pb file defining the model's parameters.
+        labels: iterable of str
+            With the labels used by our model.
+        name: str
+            The name of the model. Preferably, it should be what this model is
+            supposed to classify.
 
+        Returns
+        -------
+        machine_learning.models.Model
+            With the initialised data.
+
+        """
         with tf.gfile.GFile(pb_file, 'rb') as stream:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(stream.read())
 
-        return Model(graph_def, label_lines)
+        return Model(graph_def, labels, name)
 
     def predict(self, image):
-        img_data = tf.gfile.GFile(image, 'rb').read()
+        """Run prediction on an input image.
+
+        Parameters
+        ----------
+        image: tf.gfile.GFile
+            The data stream of the image.
+
+        Returns
+        ------
+        dict (str -> float)
+            With the prediction results, matching labels to probabilities.
+
+        """
+        result = {}
+        img_data = image.read()
 
         tf.import_graph_def(self._graph_def, name='')
 
         with tf.Session() as sess:
-            pass
+            softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+            predictions = sess.run(softmax_tensor,
+                                   {'DecodeJpeg/contents:0': img_data})
+
+            top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+            for node_id in top_k:
+                label = self._labels[node_id]
+                score = predictions[0][node_id]
+
+                result[label] = score
+
+        return result
+
+    def __str__(self):
+        return self.name
