371449b8db0df446f13854c022410611d8a6ebe5
Tudor Avram <a96tudor@gmail.com>
2018-11-25 15:03:43 +0200

    integrated with storage module

--- machine_learning/cli.py
+++ machine_learning/cli.py
@@ -2,9 +2,12 @@ import json
 import argparse
 import logging
 import sys
+import time
 
 from machine_learning.config import Config
-from machine_learning.server import ConnectAPI
+from machine_learning.models import Model
+from storage.drivers import PhotosStorageDriver
+import storage.handler_ml as handler_ml
 
 
 def get_config(path):
@@ -23,10 +26,55 @@ def get_config(path):
 def parse_args():
     parser = argparse.ArgumentParser('Rad-I/O machine learning runner.')
     parser.add_argument('config', help='Path to the config file.')
+    parser.add_argument('storage-config', help='Path to the storage config '
+                                               'file.')
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='Whether to log the workings of the API to stderr')
 
     return parser.parse_args()
+
+
+def initiate_models(config):
+    models = []
+
+    for name, data in config.models.items():
+        models.append(Model.load_from_pb(data['pb_path'], data['labels'],
+                                         name))
+
+    return models
+
+
+def run_forever(storage_handler, models):
+    """
+
+    Parameters
+    ----------
+    storage_handler: handler_ml.StorageHandler
+    models: iterable of Model
+    photos_driver: PhotosStorageDriver
+    """
+    while True:
+        logging.info('Looking for a photo to classify...')
+        requests = storage_handler.get_unused_requests()['requests']
+
+        if len(requests) == 0:
+            logging.info('None found. Going to sleep...')
+            time.sleep(2)
+            continue
+
+        logging.info('Starting to process {} requests.'.format(len(requests)))
+
+        for req in requests:
+            pid = req['photo']
+            results = {}
+
+            photo_data = storage_handler.get_photo(pid)
+
+            for model in models:
+                probs = model.predict(photo_data)
+                results[model.name] = probs
+
+            storage_handler.upload_result(results, req['id'])
 
 
 def main():
@@ -35,8 +83,9 @@ def main():
         logging.basicConfig(level=logging.INFO, stream=sys.stderr)
 
     cnf = get_config(args.config)
+    handler = handler_ml.StorageHandler(args.storage_config)
 
-    api = ConnectAPI(__name__, cnf)
+    run_forever(handler, initiate_models(cnf))
 
 
 if __name__ == '__main__':
--- machine_learning/server/utils.py
+++
@@ -1,5 +0,0 @@
-__all__ = ['config']
-
-config = None
-
-models = None
